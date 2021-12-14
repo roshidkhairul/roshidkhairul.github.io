0c11da41923b3669d0755e4e7a64f7172fcf8430
Roshid Khairul <65308980+roshidkhairul@users.noreply.github.com>
2021-12-15 01:59:38 +0600

    Add files via upload

--- assignment10script.js
+++ assignment10script.js
@@ -1,49 +1,25 @@
-// let player1Random = 0;
-let sum1 = 0;
-let sum2 = 0;
-function RandomNumberOne() {
-  let lastNumber;
-  function getRandNumber() {
-    var x = Math.floor(Math.random() * 5 + 1);
+const key = "3046d3b7ffbce27ad9bb7e2cd6d82238";
+function getNewWorkWeather(city) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
 
-    if (x === lastNumber) {
-      return getRandNumber();
-    }
-    return x;
-  }
-
-  const number = getRandNumber();
-  lastNumber = number;
-
-  sum1 = sum1 + number;
-  console.log(number);
-  document.getElementById("player1__random").innerText = number;
-  document.getElementById("player1__total").innerText = sum1;
-  if (sum1 >= 16) {
-    alert("Player one win");
-  }
-  return number;
+      let showNewWorkCity = (document.getElementById("temp").innerText =
+        data.main.temp);
+    });
 }
-function RandomNumberTwo() {
-  let lastNumber;
-  function getRandNumber() {
-    var x = Math.floor(Math.random() * 5 + 1);
+function getLosWeather(city) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
 
-    if (x === lastNumber) {
-      return getRandNumber();
-    }
-    return x;
-  }
-
-  const number = getRandNumber();
-  lastNumber = number;
-
-  sum2 = sum2 + number;
-  console.log(number);
-  document.getElementById("player2__random").innerText = number;
-  document.getElementById("player2__total").innerText = sum2;
-  if (sum2 >= 16) {
-    alert("Player two win");
-  }
-  return number;
+      let showNewWorkCity = (document.getElementById("tempLos").innerText =
+        data.main.temp);
+    });
 }
